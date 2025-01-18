0af73d545e8f4a04516b0bd075f0138cc6c6524f
Myeongkyu Lee <mgklee@kaist.ac.kr>
2025-01-18 12:55:47 +0900

    Add files via upload

---
+++ lib/tabs/tab1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Tab1 extends StatefulWidget {
+  const Tab1({super.key});
+
+  @override
+  _Tab1State createState() => _Tab1State();
+}
+
+class _Tab1State extends State<Tab1> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('This is Tab 1');
+  }
+}
