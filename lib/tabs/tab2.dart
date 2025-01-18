4fb9c225f2eb953e0514834990d0f5eba1a7ef93
Myeongkyu Lee <mgklee@kaist.ac.kr>
2025-01-18 13:22:46 +0900

    Add Tab 2 ListView

--- lib/tabs/tab2.dart
+++ lib/tabs/tab2.dart
@@ -8,8 +8,65 @@ class Tab2 extends StatefulWidget {
 }
 
 class _Tab2State extends State<Tab2> {
+  Map<String, dynamic> routines = {};
+
   @override
   Widget build(BuildContext context) {
-    return Text('This is Tab 2');
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Categories List
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: routines.length + 1, // Extra card for the (+) button
+            itemBuilder: (context, index) {
+              // (+) Button Card
+              if (index == routines.length) {
+                return Card(
+                  color: Color(0xFFF7F7F7),
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 16.0),
+                  child: Center(
+                    child: InkWell(
+                      onTap: () {
+                        // Add a new empty category with a unique name
+                        setState(() {
+                          int routineIndex = 1;
+                          String newRoutineName;
+                          do {
+                            newRoutineName = "루틴 $routineIndex";
+                            routineIndex++;
+                          } while (routines.containsKey(newRoutineName));
+
+                          routines[newRoutineName] = newRoutineName;
+                        });
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.add, size: 40.0, color: Color(0xFF18C971)
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }
+
+              return Card(
+                color: Color(0xFFF7F7F7),
+                margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                child: Column(
+                  children: [
+                    Text(routines.keys.elementAt(index)),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
