fc3f7c0dafb59a5264072673bbb6a052467b975f
gutsguy <eojin5356@gmail.com>
2025-01-18 18:57:41 +0900

    add open kakaotalk

--- lib/tabs/tab2.dart
+++ lib/tabs/tab2.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:android_intent_plus/android_intent.dart';
+import 'package:android_intent_plus/flag.dart';
+import 'package:flutter/services.dart';
+
 
 class Tab2 extends StatefulWidget {
   const Tab2({super.key});
@@ -16,6 +20,44 @@ class _Tab2State extends State<Tab2> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+
+          // 카카오톡 실행 버튼
+          _buildAppCard(
+            context,
+            icon: Icons.message,
+            label: "Open KakaoTalk",
+            onTap: () => _launchApp('com.kakao.talk'),
+          ),
+          // 카메라 실행 버튼
+          _buildAppCard(
+            context,
+            icon: Icons.camera_alt,
+            label: "Open Camera",
+            onTap: () => _launchCamera(),
+          ),
+          // 갤러리 실행 버튼
+          _buildAppCard(
+            context,
+            icon: Icons.photo,
+            label: "Open Gallery",
+            onTap: () => _launchApp('com.sec.android.gallery3d'),
+          ),
+          // 크롬 실행 버튼
+          _buildAppCard(
+            context,
+            icon: Icons.web,
+            label: "Open Chrome",
+            onTap: () => _launchApp('com.android.chrome'),
+          ),
+          // 전화 앱 실행 버튼
+          _buildAppCard(
+            context,
+            icon: Icons.phone,
+            label: "Make a Call",
+            onTap: () => _makeCall("tel:+8201086415372"),
+          ),
+
+
           // Categories List
           ListView.builder(
             shrinkWrap: true,
@@ -68,5 +110,75 @@ class _Tab2State extends State<Tab2> {
         ],
       ),
     );
+  }
+
+  Widget _buildAppCard(BuildContext context, {required IconData icon, required String label, required VoidCallback onTap}) {
+    return Card(
+      color: const Color(0xFFF7F7F7),
+      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: ListTile(
+        leading: Icon(icon, size: 40.0, color: const Color(0xFF18C971)),
+        title: Text(label, style: const TextStyle(fontSize: 16.0)),
+        onTap: onTap,
+      ),
+    );
+  }
+
+  // 앱 실행 함수
+  void _launchKakaoTalk() async {
+    final intent = AndroidIntent(
+      action: 'android.intent.action.MAIN',
+      package: 'com.kakao.talk',
+      flags: <int>[Flag.FLAG_ACTIVITY_NEW_TASK],
+    );
+    await intent.launch();
+  }
+
+  void _launchGallery() async {
+    const packageName = 'com.sec.android.gallery3d';
+    final intent = AndroidIntent(
+      action: 'android.intent.action.MAIN',
+      package: packageName,
+      flags: <int>[Flag.FLAG_ACTIVITY_NEW_TASK],
+    );
+    await intent.launch();
+  }
+
+  void _launchChrome() {
+    final intent = AndroidIntent(
+      action: 'android.intent.action.MAIN',
+      category: 'android.intent.category.LAUNCHER',
+      package: 'com.android.chrome',
+      flags: <int>[Flag.FLAG_ACTIVITY_NEW_TASK],
+    );
+    intent.launch();
+  }
+
+  void _launchApp(String packageName) {
+    final intent = AndroidIntent(
+      action: 'android.intent.action.MAIN',
+      package: packageName,
+      flags: <int>[Flag.FLAG_ACTIVITY_NEW_TASK],
+    );
+    intent.launch();
+  }
+
+  // 카메라 실행 함수
+  Future<void> _launchCamera() async {
+    final intent = AndroidIntent(
+      action: 'android.media.action.IMAGE_CAPTURE',
+      flags: <int>[Flag.FLAG_ACTIVITY_NEW_TASK],
+    );
+    await intent.launch();
+  }
+
+  // 전화 앱 실행 함수
+  Future<void> _makeCall(String phoneNumber) async {
+    final intent = AndroidIntent(
+      action: 'android.intent.action.DIAL',
+      data: phoneNumber,
+      flags: <int>[Flag.FLAG_ACTIVITY_NEW_TASK],
+    );
+    await intent.launch();
   }
 }
